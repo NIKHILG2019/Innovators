d8eff5d8d3657a8af654e860f294bc93ce8a4770
Nikhil Gangaramani <gangaramaninikhil@gmail.com>
2021-03-28 07:02:45 +0530

    added dynamic feedback, dashboard css updated, model updated, dynamic company page

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from . import db
-from .model import Company
+from .model import Company, Review
 from flask_login import login_required, current_user
 main = Blueprint("main", __name__)
 
@@ -9,10 +9,9 @@ main = Blueprint("main", __name__)
 @login_required
 def index():
     list_of_companies = db.session.query(Company).all()
-    for instatnce in list_of_companies:
-        print(instatnce.company_name)
+    for instance in list_of_companies:
+        print(instance.company_name)
     return render_template('dashboard.html', data=list_of_companies)
-
 
 
 @main.route("/subtopic")
@@ -27,17 +26,65 @@ def exam():
     return render_template('exam2.html')
 
 
-
-
 @main.route("/company", methods=['POST'])
 @login_required
 def company_post():
+    feedback_toggle = False
+    work_life_balance = 0
+    work_culture = 0
+    pay = 0
+    types_of_project = 0
+    button_toggle = True
     data = Company.query.filter_by(company_name=request.form.get('company')).first()
-    print(data.company_name)
-    return render_template('company_new.html', data=data)
+    review_list = Review.query.filter_by(company_id=data.id).all()
+    for instance in review_list:
+        if instance.user_id == current_user.id:
+            button_toggle = False
+            break
+    if int(data.number_of_feedback) != 0:
+        feedback_toggle = True
+        work_life_balance = ((int(data.feedback_work_life_balance)/5)/int(data.number_of_feedback))*100
+        work_culture = ((int(data.feedback_work_culture) / 5) / int(data.number_of_feedback)) * 100
+        pay = ((int(data.feedback_pay) / 5) / int(data.number_of_feedback)) * 100
+        types_of_project = ((int(data.feedback_types_of_project) / 5) / int(data.number_of_feedback)) * 100
+    return render_template('company_new.html', data=data, button_toggle=button_toggle, work_life_balance=work_life_balance, work_culture=work_culture, pay=pay, types_of_project=types_of_project, feedback=feedback_toggle)
 
 
 @main.route("/profile")
 @login_required
 def profile():
     return current_user.first_name
+
+
+@main.route("/feedback")
+@login_required
+def feedback():
+    data = request.args.get('company_name')
+    return render_template('feedback.html', data=data)
+
+
+@main.route('/feedback', methods=['POST'])
+@login_required
+def feedback_post():
+    data = Company.query.filter_by(company_name=request.form.get('company_name')).first()
+    data.feedback_pay = int(data.feedback_pay) + int(request.form.get('pay'))
+    data.feedback_work_life_balance = int(data.feedback_work_life_balance) + int(request.form.get('work_life_balance'))
+    data.feedback_types_of_project = int(data.feedback_types_of_project) + int(request.form.get('types_of_projects'))
+    data.feedback_work_culture = int(data.feedback_work_culture) + int(request.form.get('work_culture'))
+    data.number_of_feedback = int(data.number_of_feedback) + 1
+    new_review = Review(current_user.id, data.id)
+    db.session.add(new_review)
+    db.session.commit()
+    return redirect(url_for('main.index'))
+
+
+    # print(request.form.get('work_life_balance'))
+    # print(request.form.get('types_of_projects'))
+    # print(request.form.get('work_culture'))
+    # print(request.form.get('pay'))
+    # data.company_data["feedback_work_life_balance"] = str(int(data.company_data["feedback_work_life_balance"])+int(request.form.get('work_life_balance')))
+    # data.company_data["feedback_types_of_project"] = str(int(data.company_data["feedback_work_life_balance"])+int(request.form.get('work_life_balance')))
+    # data.company_data["feedback_work_culture"] = str(int(data.company_data["feedback_work_life_balance"])+int(request.form.get('work_life_balance')))
+    # data.company_data["feedback_pay"] = str(int(data.company_data["feedback_work_life_balance"])+int(request.form.get('work_life_balance')))
+    # data.company_data["number_of_feedback"] = str(int(data.company_data["number_of_feedback"]) + 1)
+    # data.company_data["feedback_user_id"] = data.company_data["feedback_user_id"] + ","+current_user.email
--- model.py
+++ model.py
@@ -6,29 +6,38 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
     first_name = db.Column(db.VARCHAR(20), nullable=False)
     last_name = db.Column(db.VARCHAR(20), nullable=False)
-    email = db.Column(db.VARCHAR(80), nullable=False)
+    email = db.Column(db.VARCHAR(80), unique=True, nullable=False)
     password = db.Column(db.VARCHAR(300), nullable=False)
     mobile_number = db.Column(db.BIGINT, nullable=False)
-    # email_confirmation_sent_on = db.Column(db.DATETIME, nullable=True)
-    # email_confirmed = db.Column(db.Boolean, nullable=True, default=False)
-    # email_confirmed_on = db.Column(db.DATETIME, nullable=True)
 
-    def __init__(self, first_name, last_name, email, password, mobile_number, email_confirmation_sent_on):
+    def __init__(self, first_name, last_name, email, password, mobile_number):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
         self.mobile_number = mobile_number
-        # self.email_confirmation_sent_on = email_confirmation_sent_on
-        # self.email_confirmed = False
-        # self.email_confirmed_on = None
 
 
 class Company(db.Model):
     id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
     company_name = db.Column(db.VARCHAR(50), nullable=False)
+    feedback_work_life_balance = db.Column(db.INTEGER, nullable=False, default=0)
+    feedback_types_of_project = db.Column(db.INTEGER, nullable=False, default=0)
+    feedback_work_culture = db.Column(db.INTEGER, nullable=False, default=0)
+    feedback_pay = db.Column(db.INTEGER, nullable=False, default=0)
+    number_of_feedback = db.Column(db.INTEGER, nullable=False, default=0)
     company_data = db.Column(db.JSON, nullable=False)
 
     def __init__(self, company_name, company_data):
         self.company_name = company_name,
         self.company_data = company_data
+
+
+class Review(db.Model):
+    id = db.Column(db.INTEGER, autoincrement=True, primary_key=True, nullable=False)
+    company_id = db.Column(db.INTEGER, db.ForeignKey('company.id'))
+    user_id = db.Column(db.INTEGER, db.ForeignKey('user.id'))
+
+    def __init__(self, user_id, company_id):
+        self.user_id = user_id
+        self.company_id = company_id
